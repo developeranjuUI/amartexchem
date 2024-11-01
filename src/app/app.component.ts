f73a2c04b07b5c72a66b1f26857cc09fe9c0f396
anju006 <developeranju006@gmail.com>
2024-11-01 23:28:46 +0530

    scroll to top newly added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amartexchem';
 
   isSearchHidden = true;
+  showScrollToTop = false;
 
+  constructor(private router: Router) {}
+  
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+      if(event instanceof NavigationEnd) {
+        window.scrollTo(0,0);
+      }
+    })
+  }
   toggleSearch(): void{
     this.isSearchHidden = !this.isSearchHidden;
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollToTop = window.pageYOffset > 300;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
 }
