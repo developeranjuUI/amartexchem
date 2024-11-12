ddbe6f5b3e9f4f4bfe6f5ba0689e6d79e61697fc
anju006 <developeranju006@gmail.com>
2024-11-12 17:53:46 +0530

    route updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { ContactusComponent } from './components/contactus/contactus.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'products', loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule)},
   { path: 'aboutus', component: AboutusComponent},
   { path: 'contactus', component: ContactusComponent },
-  { path: '**', redirectTo: '/home' }
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
