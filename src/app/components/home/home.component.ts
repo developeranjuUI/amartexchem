a0275266580ed8ecf0f2c01009f4ed49f2d756af
anju006 <developeranju006@gmail.com>
2024-11-24 23:16:01 +0530

    fixed issue changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,11 +9,11 @@ export class HomeComponent implements OnInit {
   slideConfig = {
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     dots: true,
     infinite: true,
-    arrows: false
+    arrows: true
   }
 
   constructor () {}
