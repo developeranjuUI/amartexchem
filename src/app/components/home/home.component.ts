0241250628b094757f6e88d39268941b04e77e01
developerAnju <116105555+developerAnju@users.noreply.github.com>
2025-02-21 12:42:25 +0530

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ContactService } from 'src/app/services/contact.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  contactForm!: FormGroup;
+  submitted = false;
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
+
   slideConfig = {
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -16,9 +24,45 @@ export class HomeComponent implements OnInit {
     arrows: true
   }
 
-  constructor () {}
+  constructor (private fb: FormBuilder, private contactService: ContactService, private toastr: ToastrService) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      mobile: ['', [Validators.required, Validators.pattern(/^[789]\d{9}$/)]],// /^[0-9]{10}$/
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.maxLength(255)]]
+    });
+  }
+  ngOnInit(): void { }
+  onSubmit() {
+    this.submitted = true;
+    this.successMessage = null;
+    this.errorMessage = null;
 
-  ngOnInit(): void {
-    
+    if(this.contactForm.valid) {
+      const formData = this.contactForm.value;
+      this.contactService.sendFormData(formData).subscribe({
+        next: (response: any) => {
+          // this.successMessage = 'Form submitted successfully!';
+          this.toastr.success('Form submitted successfully!', 'Success');
+          this.contactForm.reset();
+          this.submitted = false;
+        },
+        error: (error) => {
+          // this.errorMessage = 'Error submitting form. Please try again.';
+          this.toastr.error('Error submitting form. Please try again.', 'Error');
+        }
+      });
+    } else {
+      this.contactForm.markAllAsTouched();
+      // this.errorMessage = 'Please fill out all required fields correctly.';
+      this.toastr.warning('Please fill out all required fields correctly.', 'Invalid Form');
+    }
+  }
+  
+  onMobileInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, '');
+    input.value = value;
+    this.contactForm.get('mobile')?.setValue(value, { emitEvent: true });
   }
 }
